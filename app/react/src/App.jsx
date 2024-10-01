0893591d220e64d5bc7d06681c360c7cdc55a567
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-01 10:14:33 +0300

    Exercises1.1-1.2 Commit

---
+++ app/react/src/App.jsx
@@ -0,0 +1,51 @@
+const Head = (props) => {
+  return(
+    <h1>
+      {props.course}
+    </h1>
+  )
+}
+
+const Content = (props) =>{
+  return(
+    <Part part={props.part} exercises={props.exercises} />
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>
+      Total number of Exercise are {props.totalExecises}
+    </p>
+  )
+}
+
+const Part = (props) => { 
+  return(
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  )
+}
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <>
+        <Head course={course} />
+        <Content part={part1} exercises={exercises1} />
+        <Content part={part2} exercises={exercises2} />
+        <Content part={part3} exercises={exercises3} />
+        <Total totalExecises={exercises1+exercises2+exercises3} />
+    </>
+  )
+}
+
+export default App
