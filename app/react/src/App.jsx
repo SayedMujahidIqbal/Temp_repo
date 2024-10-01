d64636017a815cb038644a7fec4b8d3cfbf0d9b6
Mujahid Iqbal <sayedmujahidiqbal@gmail.com>
2024-10-01 10:21:54 +0300

    Exercise 1.3 Commit

--- app/react/src/App.jsx
+++ app/react/src/App.jsx
@@ -1,4 +1,4 @@
-const Head = (props) => {
+const Header = (props) => {
   return(
     <h1>
       {props.course}
@@ -30,20 +30,26 @@ const Part = (props) => {
 
 function App() {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+}
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <>
-        <Head course={course} />
-        <Content part={part1} exercises={exercises1} />
-        <Content part={part2} exercises={exercises2} />
-        <Content part={part3} exercises={exercises3} />
-        <Total totalExecises={exercises1+exercises2+exercises3} />
+        <Header course={course} />
+        <Content part={part1.name} exercises={part1.exercises} />
+        <Content part={part2.name} exercises={part2.exercises} />
+        <Content part={part3.name} exercises={part3.exercises} />
+        <Total totalExecises={part1.exercises+part2.exercises+part3.exercises} />
     </>
   )
 }
